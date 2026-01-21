c0b51440c5e8a372dd4a84a080c81df2add8892d
TalhaMalik <talha.malik@n3o.ltd>
2026-01-21 20:31:33 +0500

    Added include models

--- src/N3O.Tool/Commands/Clients/ClientsCommand.CSharp.cs
+++ src/N3O.Tool/Commands/Clients/ClientsCommand.CSharp.cs
@@ -1,7 +1,11 @@
-﻿using NJsonSchema.CodeGeneration.CSharp;
+﻿using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+using NSwag;
 using NSwag.CodeGeneration;
 using NSwag.CodeGeneration.CSharp;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace N3O.Tool.Commands.Clients; 
@@ -12,11 +16,21 @@ public partial class ClientsCommand {
 
         var settings = new CSharpClientGeneratorSettings();
 
+        var typesToRemove = new List<string>();
+        
+        if (IncludeModels?.Split('|').Any() == true) {
+            var schemas = openApiDocument.Components.Schemas;
+            
+            typesToRemove = schemas.Keys.Where(k => !IncludeModels.Split('|').Contains(k)).ToList();
+        } else if (ExcludeModels?.Split('|').Any() == true) {
+            typesToRemove.AddRange(ExcludeModels.Split('|'));
+        }
+        
         settings.ClassName = Name;
         settings.ExposeJsonSerializerSettings = true;
         settings.GenerateClientInterfaces = true;
         settings.GenerateDtoTypes = !NoModels;
-        settings.CSharpGeneratorSettings.ExcludedTypeNames = ExcludeModels?.Split('|') ?? [];
+        settings.CSharpGeneratorSettings.ExcludedTypeNames = typesToRemove.ToArray();
         settings.CSharpGeneratorSettings.GenerateOptionalPropertiesAsNullable = true;
 
         settings.CSharpGeneratorSettings.Namespace = Namespace;
@@ -28,5 +42,54 @@ public partial class ClientsCommand {
         var csClient = generator.GenerateFile(ClientGeneratorOutputType.Full);
 
         await File.WriteAllTextAsync(System.IO.Path.Combine(OutputPath, $"{Name}.cs"), csClient);
+    }
+    
+    private static HashSet<string> CollectSchemasWithDependencies(
+        OpenApiDocument document,
+        IEnumerable<string> rootSchemas) {
+
+        var result = new HashSet<string>(rootSchemas);
+        var queue = new Queue<string>(rootSchemas);
+
+        while (queue.Count > 0) {
+            var current = queue.Dequeue();
+
+            if (!document.Components.Schemas.TryGetValue(current, out var schema)) {
+                continue;
+            }
+
+            foreach (var referenced in GetReferencedSchemas(schema)) {
+                if (result.Add(referenced)) {
+                    queue.Enqueue(referenced);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static IEnumerable<string> GetReferencedSchemas(JsonSchema schema) {
+        var referencedSchemas = new List<JsonSchema>();
+
+        if (schema.Reference != null)
+            referencedSchemas.Add(schema.Reference);
+
+        referencedSchemas.AddRange(schema.AllOf);
+        referencedSchemas.AddRange(schema.OneOf);
+        referencedSchemas.AddRange(schema.AnyOf);
+
+        foreach (var property in schema.Properties.Values) {
+            if (property.Reference != null)
+                referencedSchemas.Add(property.Reference);
+
+            referencedSchemas.AddRange(property.AllOf);
+            referencedSchemas.AddRange(property.OneOf);
+            referencedSchemas.AddRange(property.AnyOf);
+        }
+
+        return referencedSchemas
+              .Where(s => s?.Title != null)
+              .Select(s => s.Title!)
+              .Distinct();
     }
 }
